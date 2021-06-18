3f2c66eeae205ad1bfb8c04e74302beb6669265a
k-matsuzawa <tangrambs803@gmail.com>
2021-06-18 16:54:08 +0900

    fix: comment.

--- service/pegin/pegin.go
+++ service/pegin/pegin.go
@@ -9,9 +9,9 @@ import (
 	"github.com/cryptogarageinc/cfd-go/types"
 )
 
-// wrap and validation APIs
+// Pegin This interface defines the API used by the pegin function.
 type Pegin interface {
-	// GetPubkeyFromExtPubkey ...
+	// GetPubkeyFromExtPubkey This function get the pubkey from xpubkey.
 	GetPubkeyFromExtPubkey(
 		extPubkey *types.ExtPubkey,
 		bip32Path string,
@@ -20,7 +20,7 @@ type Pegin interface {
 		derivedExtPubkey *types.ExtPubkey,
 		err error,
 	)
-	// CreatePeginAddress ...
+	// CreatePeginAddress This function get the pegin address and claim script.
 	CreatePeginAddress(
 		addressType types.AddressType,
 		extPubkey *types.ExtPubkey,
@@ -32,7 +32,7 @@ type Pegin interface {
 		pubkey *types.Pubkey,
 		err error,
 	)
-	// CreatePeginTransaction ...
+	// CreatePeginTransaction This function create the pegin claim transaction.
 	CreatePeginTransaction(
 		peginOutPoint *types.OutPoint,
 		peginData *types.InputPeginData,
@@ -41,13 +41,13 @@ type Pegin interface {
 		changeAddress *string,
 		option *types.FundRawTxOption,
 	) (tx *types.ConfidentialTx, err error)
-	// VerifyPubkeySignature
+	// VerifyPubkeySignature This function validate the signature by pubkey.
 	VerifyPubkeySignature(
 		proposalTx *types.Transaction,
 		utxoList *types.ElementsUtxoData,
 		signature *types.ByteData,
 	) (isVerify bool, err error)
-	// VerifyPeginPubkeySignature
+	// VerifyPubkeySignature This function validate the pegin signature by pubkey.
 	VerifyPeginPubkeySignature(
 		proposalTx *types.Transaction,
 		extPubkey *types.ExtPubkey,
@@ -56,33 +56,21 @@ type Pegin interface {
 	) (isVerify bool, err error)
 }
 
+// NewPeginService This function returns an object that defines the API for Pegin.
 func NewPeginService() *PeginService {
 	return &PeginService{}
 }
 
 // -------------------------------------
-// PeginUtil
+// PeginService
 // -------------------------------------
 
+// PeginService This struct is implements pegin api.
 type PeginService struct {
 	Network *types.NetworkType
 }
 
-func (p *PeginService) validConfig() error {
-	if p.Network == nil {
-		cfdConfig := config.GetCurrentCfdConfig()
-		if !cfdConfig.Network.Valid() {
-			return fmt.Errorf("CFD Error: NetworkType not set")
-		}
-		netType := cfdConfig.Network
-		p.Network = &netType
-	}
-	if !p.Network.IsElements() {
-		return fmt.Errorf("CFD Error: NetworkType is not elements")
-	}
-	return nil
-}
-
+// GetPubkeyFromExtPubkey This function get the pubkey from xpubkey.
 func (p *PeginService) GetPubkeyFromExtPubkey(
 	extPubkey *types.ExtPubkey,
 	bip32Path string,
@@ -94,6 +82,8 @@ func (p *PeginService) GetPubkeyFromExtPubkey(
 	if err = p.validConfig(); err != nil {
 		return nil, nil, err
 	}
+	// FIXME implements extPubkey validation
+	// FIXME implements bip32Path validation
 	deriveKey, err := cfd.CfdGoCreateExtkeyFromParentPath(extPubkey.Key, bip32Path, p.Network.ToCfdValue(), int(cfd.KCfdExtPubkey))
 	if err != nil {
 		return nil, nil, err
@@ -102,9 +92,11 @@ func (p *PeginService) GetPubkeyFromExtPubkey(
 	if err != nil {
 		return nil, nil, err
 	}
+	// FIXME implements depth check
 	return &types.Pubkey{Hex: pubkeyHex}, &types.ExtPubkey{Key: deriveKey}, nil
 }
 
+// CreatePeginAddress This function get the pegin address and claim script.
 func (p *PeginService) CreatePeginAddress(
 	addressType types.AddressType,
 	extPubkey *types.ExtPubkey,
@@ -119,10 +111,21 @@ func (p *PeginService) CreatePeginAddress(
 	if err = p.validConfig(); err != nil {
 		return nil, nil, nil, err
 	}
+	switch addressType {
+	case types.P2shAddress:
+	case types.P2wshAddress:
+	case types.P2shP2wshAddress:
+		break
+	default:
+		return nil, nil, nil, fmt.Errorf("CFD Error: Invalid pegin address type")
+	}
+	// FIXME implements extPubkey validation
+	// FIXME implements bip32Path validation
 	deriveKey, err := cfd.CfdGoCreateExtkeyFromParentPath(extPubkey.Key, bip32Path, p.Network.ToCfdValue(), int(cfd.KCfdExtPubkey))
 	if err != nil {
 		return nil, nil, nil, err
 	}
+	// FIXME implements depth check
 	pubkeyHex, err := cfd.CfdGoGetPubkeyFromExtkey(deriveKey, p.Network.ToCfdValue())
 	if err != nil {
 		return nil, nil, nil, err
@@ -135,6 +138,7 @@ func (p *PeginService) CreatePeginAddress(
 	return peginAddress, claimScript, &types.Pubkey{Hex: pubkeyHex}, nil
 }
 
+// CreatePeginTransaction This function create the pegin claim transaction.
 func (p *PeginService) CreatePeginTransaction(
 	peginOutPoint *types.OutPoint,
 	peginData *types.InputPeginData,
@@ -151,22 +155,41 @@ func (p *PeginService) CreatePeginTransaction(
 	return nil, nil
 }
 
+// VerifyPubkeySignature This function validate the signature by pubkey.
 func (p *PeginService) VerifyPubkeySignature(
 	proposalTx *types.Transaction,
 	utxoList *types.ElementsUtxoData,
 	signature *types.ByteData,
 ) (isVerify bool, err error) {
+	// FIXME implements
 	// 1. pegin: auto-get amount etc.
 	// 2. other utxo: normal verify
 	return false, nil
 }
 
+// VerifyPubkeySignature This function validate the pegin signature by pubkey.
 func (p *PeginService) VerifyPeginPubkeySignature(
 	proposalTx *types.Transaction,
 	extPubkey *types.ExtPubkey,
 	bip32Path string,
 	signature *types.ByteData,
 ) (isVerify bool, err error) {
+	// FIXME implements
 	// 1. pegin: auto-get amount etc.
 	return false, nil
+}
+
+func (p *PeginService) validConfig() error {
+	if p.Network == nil {
+		cfdConfig := config.GetCurrentCfdConfig()
+		if !cfdConfig.Network.Valid() {
+			return fmt.Errorf("CFD Error: NetworkType not set")
+		}
+		netType := cfdConfig.Network
+		p.Network = &netType
+	}
+	if !p.Network.IsElements() {
+		return fmt.Errorf("CFD Error: NetworkType is not elements")
+	}
+	return nil
 }
--- service/pegout/pegout.go
+++ service/pegout/pegout.go
@@ -1,22 +1,23 @@
-package pegin
+package pegout
 
 import (
 	"fmt"
 
+	cfd "github.com/cryptogarageinc/cfd-go"
 	"github.com/cryptogarageinc/cfd-go/config"
 	"github.com/cryptogarageinc/cfd-go/types"
 )
 
-// wrap and validation APIs
+// Pegout This interface defines the API used by the pegout function.
 type Pegout interface {
-	// CreateOnliePrivateKey ...
-	CreateOnliePrivateKey() (privkey *types.Privkey, err error)
-	// CreatePakEntry
+	// CreateOnlinePrivateKey This function generate random private key for online key.
+	CreateOnlinePrivateKey() (privkey *types.Privkey, err error)
+	// CreatePakEntry This function create the PAK-Entry.
 	CreatePakEntry(
 		extPubkey *types.ExtPubkey,
 		onlinePrivkey *types.Privkey,
 	) (pakEntry *types.ByteData, err error)
-	// CreatePeginAddress ...
+	// CreatePegoutAddress This function create the pegout address for bitcoin network.
 	CreatePegoutAddress(
 		addressType types.AddressType,
 		extPubkey *types.ExtPubkey,
@@ -25,7 +26,7 @@ type Pegout interface {
 		pegoutAddress *types.Address,
 		err error,
 	)
-	// CreatePegoutTransaction ...
+	// CreatePegoutTransaction This function create the pegout transaction.
 	CreatePegoutTransaction(
 		utxoList []types.ElementsUtxoData,
 		pegoutData types.InputConfidentialTxOut,
@@ -33,7 +34,7 @@ type Pegout interface {
 		changeAddress *string,
 		option *types.FundRawTxOption,
 	) (tx *types.ConfidentialTx, err error)
-	// VerifyPubkeySignature
+	// VerifyPubkeySignature This function validate the signature by pubkey.
 	VerifyPubkeySignature(
 		proposalTx *types.Transaction,
 		utxoList *types.ElementsUtxoData,
@@ -41,55 +42,106 @@ type Pegout interface {
 	) (isVerify bool, err error)
 }
 
+// NewPegoutService This function returns an object that defines the API for Pegout.
 func NewPegoutService() *PegoutService {
 	return &PegoutService{}
 }
 
 // -------------------------------------
-// PeginUtil
+// PegoutService
 // -------------------------------------
 
+// PegoutService This struct is implements pegout api.
 type PegoutService struct {
 	Network *types.NetworkType
 }
 
-func (p *PegoutService) validConfig() error {
-	if p.Network == nil {
-		cfdConfig := config.GetCurrentCfdConfig()
-		if !cfdConfig.Network.Valid() {
-			return fmt.Errorf("CFD Error: NetworkType not set")
-		}
-		netType := cfdConfig.Network
-		p.Network = &netType
+// CreateOnlinePrivateKey This function generate random private key for online key.
+func (p *PegoutService) CreateOnlinePrivateKey() (privkey *types.Privkey, err error) {
+	if err = p.validConfig(); err != nil {
+		return nil, err
 	}
-	if !p.Network.IsElements() {
-		return fmt.Errorf("CFD Error: NetworkType is not elements")
+	_, privkeyHex, wif, err := cfd.CfdGoCreateKeyPair(true, p.Network.ToBitcoinType().ToCfdValue())
+	if err != nil {
+		return nil, err
 	}
-	return nil
+	return &types.Privkey{
+		Hex:                privkeyHex,
+		Wif:                wif,
+		Network:            *p.Network,
+		IsCompressedPubkey: true,
+	}, nil
 }
 
-func (p *PegoutService) CreateOnliePrivateKey() (privkey *types.Privkey, err error) {
-	return nil, nil
-}
-
+// CreatePakEntry This function create the PAK-Entry.
 func (p *PegoutService) CreatePakEntry(
 	extPubkey *types.ExtPubkey,
 	onlinePrivkey *types.Privkey,
 ) (pakEntry *types.ByteData, err error) {
-	return nil, err
+	if err = p.validConfig(); err != nil {
+		return nil, err
+	}
+	if err = validateExtPubkey(extPubkey); err != nil {
+		return nil, err
+	}
+	// FIXME implements privkey validation
+
+	offlinePubkey, err := cfd.CfdGoGetPubkeyFromExtkey(extPubkey.Key, p.Network.ToBitcoinType().ToCfdValue())
+	if err != nil {
+		return nil, err
+	}
+	offlineNegatePubkey, err := cfd.CfdGoNegatePubkey(offlinePubkey)
+	if err != nil {
+		return nil, err
+	}
+	// FIXME implements privkey wif pattern
+	onlinePubkey, err := cfd.CfdGoGetPubkeyFromPrivkey(onlinePrivkey.Hex, "", true)
+	if err != nil {
+		return nil, err
+	}
+	pakEntryObj, err := types.NewByteDataFromHex(offlineNegatePubkey + onlinePubkey)
+	if err != nil {
+		return nil, err
+	}
+	return &pakEntryObj, nil
 }
 
+// CreatePegoutAddress This function create the pegout address for bitcoin network.
 func (p *PegoutService) CreatePegoutAddress(
 	addressType types.AddressType,
 	extPubkey *types.ExtPubkey,
 	accountIndex uint32,
-) (
-	pegoutAddress *types.Address,
-	err error,
-) {
-	return nil, err
+) (pegoutAddress *types.Address, err error) {
+	if err = p.validConfig(); err != nil {
+		return nil, err
+	}
+	switch addressType {
+	case types.P2pkhAddress:
+	case types.P2wpkhAddress:
+	case types.P2shP2wpkhAddress:
+		break
+	default:
+		return nil, fmt.Errorf("CFD Error: Invalid pegout address type")
+	}
+	if err = validateExtPubkey(extPubkey); err != nil {
+		return nil, err
+	}
+	if accountIndex >= 0x80000000 {
+		return nil, fmt.Errorf("CFD Error: Invalid account index. The hardened index can not used on the pegout")
+	}
+
+	address, _, err := cfd.CfdGoGetPegoutAddress(p.Network.ToBitcoinType().ToCfdValue(), p.Network.ToCfdValue(), extPubkey.Key, accountIndex, addressType.ToCfdValue())
+	if err != nil {
+		return nil, err
+	}
+	return &types.Address{
+		Address: address,
+		Network: p.Network.ToBitcoinType(),
+		Type:    addressType,
+	}, nil
 }
 
+// CreatePegoutTransaction This function create the pegout transaction.
 func (p *PegoutService) CreatePegoutTransaction(
 	utxoList []types.ElementsUtxoData,
 	pegoutData types.InputConfidentialTxOut,
@@ -108,10 +160,41 @@ func (p *PegoutService) CreatePegoutTransaction(
 	return nil, nil
 }
 
+// VerifyPubkeySignature This function validate the signature by pubkey.
 func (p *PegoutService) VerifyPubkeySignature(
 	proposalTx *types.Transaction,
 	utxoList *types.ElementsUtxoData,
 	signature *types.ByteData,
 ) (isVerify bool, err error) {
+	// FIXME implements
 	return false, nil
+}
+
+func (p *PegoutService) validConfig() error {
+	if p.Network == nil {
+		cfdConfig := config.GetCurrentCfdConfig()
+		if !cfdConfig.Network.Valid() {
+			return fmt.Errorf("CFD Error: NetworkType not set")
+		}
+		netType := cfdConfig.Network
+		p.Network = &netType
+	}
+	if !p.Network.IsElements() {
+		return fmt.Errorf("CFD Error: NetworkType is not elements")
+	}
+	return nil
+}
+
+func validateExtPubkey(extPubkey *types.ExtPubkey) error {
+	if extPubkey == nil {
+		return fmt.Errorf("CFD Error: Pegout extkey is null")
+	}
+	data, err := cfd.CfdGoGetExtkeyInformation(extPubkey.Key)
+	if err != nil {
+		return err
+	}
+	if data.Depth != 3 {
+		return fmt.Errorf("CFD Error: Invalid pegout extkey depth")
+	}
+	return nil
 }
