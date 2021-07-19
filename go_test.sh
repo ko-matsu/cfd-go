ad31cadfe6fefc9fb658bb1c458a9d87aecb62e8
k-matsuzawa <tangrambs803@gmail.com>
2021-07-19 16:39:49 +0900

    chore(deps): add path

--- go_test.sh
+++ go_test.sh
@@ -4,4 +4,4 @@ GO_EXEC_PATH=go
 fi
 LIB_PATH=./build/Release:../build/Release:../../build/Release
 
-LD_LIBRARY_PATH=$LIB_PATH $GO_EXEC_PATH test . ./types/... ./errors ./utils ./apis/... ./service/... ./tests -v
+LD_LIBRARY_PATH=$LIB_PATH $GO_EXEC_PATH test . ./types/... ./errors ./utils ./config ./apis/... ./service/... ./tests -v
