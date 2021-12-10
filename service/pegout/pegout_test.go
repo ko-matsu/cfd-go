a20a203e0cafb9c1bdaf4e672135df020f6ba5ab
k-matsuzawa <tangrambs803@gmail.com>
2021-12-10 09:28:49 +0900

    test: add max pegout

--- service/pegout/pegout_test.go
+++ service/pegout/pegout_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	cfdgo "github.com/cryptogarageinc/cfd-go"
 	"github.com/cryptogarageinc/cfd-go/apis/address"
 	"github.com/cryptogarageinc/cfd-go/apis/descriptor"
 	"github.com/cryptogarageinc/cfd-go/apis/key"
@@ -280,6 +281,147 @@ func TestCreatePegoutTxSegwit(t *testing.T) {
 	assert.True(t, isVerify)
 	assert.Equal(t, "", reason)
 	// assert.Equal(t, "", tx.Hex)
+
+	fmt.Printf("%s test done.\n", GetFuncName())
+}
+
+func TestCreatePegoutTxMaximumWhitelist(t *testing.T) {
+	config.SetCfdConfig(config.CfdConfig{
+		Network:                 types.NewNetworkTypeByString("liquidv1"),
+		BitcoinGenesisBlockHash: "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206",
+		BitcoinAssetId:          "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225",
+	})
+
+	// pegoutApi := (Pegout)(NewPegoutService())
+	// keyApi := (key.PrivkeyApi)(key.NewPrivkeyApi())
+	xprvApi := (key.ExtPrivkeyApi)(key.NewExtPrivkeyApi())
+	privkeyApi := (key.PrivkeyApi)(key.NewPrivkeyApi())
+	txApi := (transaction.ConfidentialTxApi)(transaction.NewConfidentialTxApi())
+	pegoutApi := (Pegout)(NewPegoutService())
+
+	// key
+	// root: xprv9s21ZrQH143K4SS9fUBooJcNan78y4SxCHjma2238tm8pGourqqBZh6pDJHEkksojBRQU4m4kgB1n1dK98tKHKPjxnLyLCUNRK7RgyqDZj7
+	accountExtPriv := types.ExtPrivkey{
+		Key: "xprv9zFUjcmCAhj2mYvQk1AAJGdrbMTciiBhabGLwLRtMuWjKu7Ab9qUvsjcySjGXZqjWHcZWyKRb92RXcXtCrj541Rr9vDv6WMrZ2vdbMQ98sZ"}
+	utxoPath := "0/10"
+	utxoExtPriv, err := xprvApi.GetExtPrivkeyByPath(&accountExtPriv, utxoPath)
+	assert.NoError(t, err)
+	utxoPubkey, err := xprvApi.GetPubkey(utxoExtPriv)
+	assert.NoError(t, err)
+	assert.Equal(t, "03e68167b077f06fdcef2b1c4b914df53fcdc4ea2ed43852cc3c2abf2b7992b729", utxoPubkey.Hex)
+	utxoPrivkey, err := xprvApi.GetPrivkey(utxoExtPriv)
+	assert.NoError(t, err)
+	assert.Equal(t, "0d96bb6416bf243e35a9969316cbd303e5204be3fbce05c96b8bbc5d7a392c67", utxoPrivkey.Hex)
+	assert.Equal(t, "Kwg8FCSKWKdwyKzYTheBAN2SvSNCSCudHBDYJBodidoSsXskGQ3S", utxoPrivkey.Wif)
+
+	onlinePrivkeyWif := "L52AgshDAE14NHJuovwAw8hyrTNK4YQjuiPC9EES4sfM7oBPzU4o"
+	onlinePrivkey, err := privkeyApi.GetPrivkeyFromWif(onlinePrivkeyWif)
+	// pegoutApi.CreateOnlinePrivateKey()  // generate random privkey
+	assert.NoError(t, err)
+
+	// mainchain address descriptor
+	// m/44h/0h/1h
+	mainchainXpubkey := types.ExtPubkey{Key: "xpub6DEq98J615HL2A5UXP5DVPmEtet7DXAsqQHEBvfbEcwAC9PBKu9cG3tCkU5fXkiaJkeQzc81YiY6DDUg82eGx2dr7NpvBXstZvw5M6wisVo"}
+	addressIndex := uint32(0)
+
+	// whitelist
+	pakEntry, err := pegoutApi.CreatePakEntry(&mainchainXpubkey, onlinePrivkey)
+	assert.NoError(t, err)
+	whitelist := ""
+	// pks := make([]string, 255) // maximum over. (current: pass by zkp's bug)
+	pks := make([]string, 254)
+	for i := range pks {
+		pk1, _, _, err := cfdgo.CfdGoCreateKeyPair(true, types.Mainnet.ToCfdValue())
+		assert.NoError(t, err)
+		pk2, _, _, err := cfdgo.CfdGoCreateKeyPair(true, types.Mainnet.ToCfdValue())
+		assert.NoError(t, err)
+		pks[i] = pk1 + pk2
+		whitelist += pks[i]
+	}
+	whitelist += pakEntry.ToHex()
+
+	// pegout address
+	pegoutAddr, desc, err := pegoutApi.CreatePegoutAddress(types.P2pkhAddress, &mainchainXpubkey, addressIndex)
+	assert.NoError(t, err)
+	assert.Equal(t, "1D4YiPF4k9qotSS3QWMa2E8Bt4jV9SZPmE", pegoutAddr.Address)
+	assert.Equal(t, "pkh(xpub6DEq98J615HL2A5UXP5DVPmEtet7DXAsqQHEBvfbEcwAC9PBKu9cG3tCkU5fXkiaJkeQzc81YiY6DDUg82eGx2dr7NpvBXstZvw5M6wisVo)", desc.OutputDescriptor)
+
+	// create pegout tx
+	pegoutData := types.InputConfidentialTxOut{
+		Amount: 1000000000,
+		PegoutInput: &types.InputPegoutData{
+			OnlineKey:               onlinePrivkey.Hex,
+			BitcoinOutputDescriptor: desc.OutputDescriptor,
+			Bip32Counter:            addressIndex,
+			Whitelist:               whitelist,
+		},
+	}
+	utxos := []*types.ElementsUtxoData{
+		{
+			OutPoint: types.OutPoint{
+				Txid: "4aa201f333e80b8f62ba5b593edb47b4730212e2917b21279f389ba1c14588a3",
+				Vout: 0,
+			},
+			Amount:           20000000000,
+			Asset:            "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225",
+			Descriptor:       "wpkh([d7f351ee/" + utxoPath + "]" + utxoPubkey.Hex + ")",
+			AssetBlindFactor: "95e6e0912047f088394be103f3a1761adcbd92466abfe41f0964a3aa2fc201e5",
+			ValueBlindFactor: "55bf185ddc2d1c747da2a82b8c9954179edec0af886daaf98d8a7b862e78bcee",
+			AmountCommitment: "08b760fd74cae28eaa41126b3c1129b2d708d893e17b4e61bd9d5a5b12a1c7643b",
+		},
+	}
+	changeAddress := "lq1qqwqawne0jyc2swqv9qp8fstrgxuux2824zxkqew9gdak4yudxvwhha0kwdv2p3j0lyekhchrzmuekp94fpfp6fkeggjkerfr8"
+	option := types.NewPegoutTxOption()
+	option.KnapsackMinChange = 0
+	tx, pegoutAddr, unblindTx, err := pegoutApi.CreatePegoutTransaction(utxos, pegoutData, nil, &changeAddress, &option)
+	assert.NoError(t, err)
+	assert.Equal(t, "1D4YiPF4k9qotSS3QWMa2E8Bt4jV9SZPmE", pegoutAddr.Address)
+
+	// output check
+	_, inList, outList, err := txApi.GetAll(tx, false)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(inList))
+	assert.Equal(t, 3, len(outList)) // pegout, fee, output(change)
+	assert.Less(t, 23110, len(tx.Hex))
+	assert.Greater(t, 23120, len(tx.Hex))
+	_, _, unblindTxoutList, err := txApi.GetAll(unblindTx, false)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1000000000), unblindTxoutList[0].Amount)
+	assert.Equal(t, int64(1404), unblindTxoutList[1].Amount)
+	assert.Equal(t, int64(18999998596), unblindTxoutList[2].Amount)
+
+	pegoutAddress, hasPegout, err := txApi.GetPegoutAddress(tx, uint32(0))
+	assert.NoError(t, err)
+	assert.True(t, hasPegout)
+	assert.Equal(t, pegoutAddr.Address, pegoutAddress.Address)
+
+	// get sighash
+	signUtxos, err := txApi.FilterUtxoByTxInList(tx, utxos)
+	assert.NoError(t, err)
+	utxoDesc := &types.Descriptor{OutputDescriptor: signUtxos[0].Descriptor}
+	sighash, err := txApi.GetSighash(tx, &utxos[0].OutPoint, types.SigHashTypeAll, signUtxos)
+	assert.NoError(t, err)
+
+	// calc signature
+	signature, err := privkeyApi.CreateEcSignature(utxoPrivkey, sighash, &types.SigHashTypeAll)
+	assert.NoError(t, err)
+
+	// verify signature
+	isVerify, err := pegoutApi.VerifyPubkeySignature(tx, utxos[0], signature)
+	assert.NoError(t, err)
+	assert.True(t, isVerify)
+
+	// add sign
+	err = txApi.AddPubkeySignByDescriptor(tx, &utxos[0].OutPoint, utxoDesc, signature.ToHex())
+	assert.NoError(t, err)
+
+	// verify (after sign)
+	isVerify, reason, err := txApi.VerifySign(tx, &utxos[0].OutPoint, signUtxos)
+	assert.NoError(t, err)
+	assert.True(t, isVerify)
+	assert.Equal(t, "", reason)
+	// assert.Equal(t, "", tx.Hex)
+	// assert.Equal(t, "", whitelist)
 
 	fmt.Printf("%s test done.\n", GetFuncName())
 }
